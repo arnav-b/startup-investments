e701265b5d241cbb25a580b6604df24e99a1415b
Arnav Burudgunte <ab141@rice.edu>
2020-11-22 14:24:50 -0600

    Create subset of acquisitions graph

--- acquisitions.py
+++ acquisitions.py
@@ -2,86 +2,58 @@
 Graph visualization of acquisitions.
 """
 
-import pandas as pd
-import plotly.graph_objects as go
+import pandas as pd 
+import plotly.graph_objects as go 
 import networkx as nx 
 
-def add_edge(graph, edge, edge_x, edge_y):
-	"""
-	Add coordinates of edge to edge_x and edge_y
-	"""
-	x0, y0 = graph.nodes[edge[0]]["pos"]
-	x1, y1 = graph.nodes[edge[1]]["pos"]
-	edge_x.extend([x0, x1, None])
-	edge_y.extend([y0, y1, None])
+acquisitions = pd.read_csv("data/acquisitions.csv")
+acquisitions = acquisitions.head(200)
 
-def create_edge_trace(graph):
-	"""
-	Create all edges as disconnected lines.
-	"""
-	edge_x, edge_y = [], []
-	for edge in graph.edges():
-		add_edge(graph, edge, edge_x, edge_y)	
+G = nx.from_pandas_edgelist(acquisitions, "acquired_object_id", "acquiring_object_id",
+	["price_amount", "price_currency_code", "acquired_at"])
+
+pos_dict = nx.spring_layout(G)
+for n, p in pos_dict.items():
+	G.nodes[n]["pos"] = p
+
+edge_x, edge_y = [], []
+for edge in G.edges():
+    x0, y0 = G.nodes[edge[0]]["pos"]
+    x1, y1 = G.nodes[edge[1]]["pos"]
+    edge_x.extend([x0, x1, None])
+    edge_y.extend([y0, y1, None])	
 	 
-	edge_trace = go.Scatter(
-		x = [],
-		y = [],
-		line = dict(width=0.5, color = "#888"),
-		hoverinfo = "text",
-		mode = "lines")
-	
-	return edge_trace	
+edge_trace = go.Scatter(
+	x = [],
+	y = [],
+	line = dict(width=0.5, color = "#888"),
+	hoverinfo = "text",
+	mode = "lines")
 
-def create_node_trace(graph):
-	"""
-	Creates all nodes as scatter traces.
-	"""
-	node_x, node_y = [], []
-	for node in graph.nodes():
-		x, y = graph.nodes[node]["pos"]
-		node_x.append(x)
-		node_y.append(y)
+node_x, node_y = [], []
+for node in G.nodes():
+	x, y = G.nodes[node]["pos"]
+	node_x.append(x)
+	node_y.append(y)
 	
-	node_trace = go.scatter(
-		x = node_x, y = node_y,
-		mode = "markers",
-		hoverinfo = "text",
-		marker = dict(
-			showscale = True,
-			colorscale = "YlOrRd",
-			reversescale = True,
-			color = [],
-			size = 10,
-			colorbar = dict(
-				thickness = 15,
-				title = "Number of Acquisitions",
-				xanchor = "left",
-				titleslide = "right"),
-		line_width = 2))
-	
-	return node_trace
+node_trace = go.Scatter(
+	x = node_x, y = node_y,
+	mode = "markers",
+	hoverinfo = "text",
+	marker = dict(
+		showscale = True,
+		colorscale = "YlOrRd",
+		reversescale = True,
+		color = [],
+		size = 10,
+		colorbar = dict(
+			thickness = 15,
+			title = "Number of Acquisitions",
+			xanchor = "left",
+			titleside = "right"),
+	line_width = 2))
 
-def color_nodes(graph):
-	"""
-	Color nodes by number of acquisitions.
-	"""
-	pass
-
-def run():
-	acquisitions = pd.read_csv("data/acquisitions.csv")
-	acquisitions = acquisitions.head(200)
-
-	G = nx.from_pandas_edgelist(acquisitions, "acquired_object_id", "acquiring_object_id",
-		["price_amount", "price_currency_code", "acquired_at"])
-
-	pos_dict = nx.spring_layout(G)
-	for n, p in pos_dict.items():
-		G.nodes[n]["pos"] = p
-	
-	edge_trace = create_edge_trace(G)
-	node_trace = create_node_trace(G)
-	
-	fig = go.Figure(data = [edge_trace, node_trace],
+fig = go.Figure(data = [edge_trace, node_trace],
 		layout = go.Layout(
 			title = "<br>Acquisitions",
 			titlefont_size = 16,
@@ -91,6 +63,4 @@ def run():
 			xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
             yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
 
-	fig.write_html("acquisitions.html")
-
-run()
+fig.write_html("acquisitions.html")
